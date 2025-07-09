ef409e2e488d29cdbb0e2233015c973866579de3
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-07-09 15:19:05 -0600

    added class methods for add to genre and add to artist dict counts

--- lib/song.py
+++ lib/song.py
@@ -2,8 +2,8 @@ class Song:
     count = 0
     genres = []
     artists = []
-    genre_count = {}
-    artist_count = {}
+    genre_count = {"Rap": 5, "Rock": 1, "Country": 3}
+    artist_count = {"Beyonce": 17, "Jay-Z": 40}
 
     def __init__(self, name, artist, genre):
         self.name = name
@@ -13,6 +13,8 @@ class Song:
         Song.add_song_to_count()
         Song.add_to_genres(genre)
         Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artists_count(artist)
 
     @classmethod
     def add_song_to_count(cls):
@@ -33,10 +35,16 @@ class Song:
             return
 
     @classmethod
-    def add_to_genre_count(cls):
-        pass
+    def add_to_genre_count(cls, genre):
+        if genre not in cls.genre_count:
+            cls.genre_count[genre] = 1
+        else:
+            cls.genre_count[genre] += 1
 
     @classmethod
-    def add_to_artists_count(cls):
-        pass
+    def add_to_artists_count(cls, artist):
+        if artist not in cls.artist_count:
+            cls.artist_count[artist] = 1
+        else:
+            cls.artist_count[artist] += 1
 
