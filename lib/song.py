91f5dfa4509ca011c50007ef5a2b733aa4f8fa8f
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-07-09 14:46:06 -0600

    set up song class and added class method for add song to count

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,34 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.add_song_to_count()
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls):
+        pass
+
+    @classmethod
+    def add_to_artists(cls):
+        pass
+
+    @classmethod
+    def add_to_genre_count(cls):
+        pass
+
+    @classmethod
+    def add_to_artists_count(cls):
+        pass
+
